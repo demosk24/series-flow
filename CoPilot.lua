b882c999e49e0947c96a8b63a75de7b93e49f318
demosk24 <demosk24@gmail.com>
2025-09-13 10:43:55 +0600

    Update CoPilot.lua

--- CoPilot.lua
+++ CoPilot.lua
@@ -19,39 +19,8 @@ period = 3
 up_color  = rgba(50, 205, 50,0.3)
 down_color = rgba(255, 87, 51,0.3)
 
-local reference = make_series ()
-reference:set(nz(reference[1], high))
-
-local is_direction_up = make_series ()
-is_direction_up:set(nz(is_direction_up[1], true))
-
-local htrack = make_series ()
-local ltrack = make_series ()
-
-local pivot = make_series ()
-
-reverse_range = reference * percentage / 100
-
-if get_value (is_direction_up) then
-    htrack:set (max(high, nz(htrack[1], high)))
-
-    if close < htrack[1] - reverse_range then
-        pivot:set (htrack)
-        is_direction_up:set (false)
-        reference:set(htrack)
-    end
-else
-    ltrack:set (min(low, nz(ltrack[1], low)))
-
-    if close > ltrack[1] + reverse_range then
-        pivot:set (ltrack)
-        is_direction_up:set(true)
-        reference:set (ltrack)
-    end
-end
-
-color = is_direction_up() and  up_color or down_color
-
+BBUpper1 = sma(close, 20) + stdev(close, 20) * 1
+BBLower1 = sma(close, 20) - stdev(close, 20) * 1
 --===NEW AREA=============================================================================
 sec = security (current_ticker_id, "1m")
 
@@ -63,7 +32,10 @@ if (sec ~= nil) and (sec.open_time == open_time) then
  end
 end
 
-plot(pivot, 'ZZ', color, 1, -1, style.solid_line, na_mode.continue)
+plot(BBUpper1,'BBUpper1','rgba(211, 211, 211, 0.3)',0.5,0,style.solid_line)
+plot(sma(close, 20),'Sma20','rgba(211, 211, 211, 0.3)',0.5,0,style.solid_line)
+plot(BBLower1,'BBLower1','rgba(211, 211, 211, 0.3)',0.5,0,style.solid_line)
+
 
 plot(ema20,'Ema20','rgba(250, 233, 33, 0.3)',0.5,0,style.solid_line)
 plot(ema50,'Ema50','rgba(202, 110, 16, 0.3)',0.5,0,style.solid_line)
